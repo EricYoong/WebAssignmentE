53d0ece64cacde50fb49fc68cd3624ab95522d44
devilking9820@hotmail.com <devilking9820@hotmail.com>
2018-08-06 12:14:34 +0800

    Add some function

--- WEbAssignment/js/WebAssignment.js
+++ WEbAssignment/js/WebAssignment.js
@@ -1,20 +1,3 @@
- function cycleBackgrounds() {
-     var index = 0;
-
-     $imageEls = $('.container .slide'); // Get the images to be cycled.
-
-     setInterval(function () {
-         // Get the next index.  If at end, restart to the beginning.
-         index = index + 1 < $imageEls.length ? index + 1 : 0;
-
-         // Show the next
-         $imageEls.eq(index).addClass('show');
-
-         // Hide the previous
-         $imageEls.eq(index - 1).removeClass('show');
-     }, 2000);
- };
-
  (function ($) {
      "use strict"; // Start of use strict
 
@@ -31,7 +14,4 @@
              }
          }
      });
-
-     cycleBackgrounds();
-
  })(jQuery); // End of use strict
