73cb4bb51b9dd97bced317c5f87743a847e8e5d3
HaoHao <hao5566hao@gmail.com>
2018-08-15 21:30:31 +0800

    added firebase

---
+++ WEbAssignment/jquery/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDQJKbVvF6Wl7NYeiSWcuhrpnBFo5Qkf-w",
+    authDomain: "webassignment666.firebaseapp.com",
+    databaseURL: "https://webassignment666.firebaseio.com",
+    projectId: "webassignment666",
+    storageBucket: "webassignment666.appspot.com",
+    messagingSenderId: "224824077875"
+  };
+  firebase.initializeApp(config);
+</script>
