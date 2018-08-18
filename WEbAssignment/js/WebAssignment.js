76795fb608c1425685c5782e963d6aff50da348e
devilking9820@hotmail.com <devilking9820@hotmail.com>
2018-08-18 22:22:49 +0800

    add some function

--- WEbAssignment/js/WebAssignment.js
+++ WEbAssignment/js/WebAssignment.js
@@ -33,4 +33,34 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
+    /* Preloader
+     * -------------------------------------------------- */
+    var clPreloader = function () {
+
+        $("html").addClass('cl-preload');
+
+        $(window).on('load', function () {
+
+            //force page scroll position to top at page refresh
+            // $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+            // will first fade out the loading animation
+            $("#loader").fadeOut("slow", function () {
+                // will fade out the whole DIV that covers the website.
+                $("#preloader").delay(300).fadeOut("slow");
+            });
+
+            // for hero content animations
+            $("html").removeClass('cl-preload');
+            $("html").addClass('cl-loaded');
+
+        });
+    };
+
+    (function ssInit() {
+
+        clPreloader();
+
+    })();
+
 })(jQuery); // End of use strict
