2802bff35ecfb34cbd48bf774c9ffdc4a2c94e05
devilking9820@hotmail.com <devilking9820@hotmail.com>
2018-08-19 02:20:58 +0800

    add some function

--- WEbAssignment/js/WebAssignment.js
+++ WEbAssignment/js/WebAssignment.js
@@ -45,9 +45,9 @@
             // $('html, body').animate({ scrollTop: 0 }, 'normal');
 
             // will first fade out the loading animation
-            $("#loader").fadeOut("slow", function () {
+            $(".loader-inner").fadeOut("slow", function () {
                 // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
+                $(".loader").delay(300).fadeOut("slow");
             });
 
             // for hero content animations
