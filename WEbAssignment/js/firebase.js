7d6f3a57e024815fdf411a45d7a6473a31630278
Eric <devilking9820@hotmail.com>
2018-08-20 08:07:03 +0800

    Add some function

--- WEbAssignment/js/firebase.js
+++ WEbAssignment/js/firebase.js
@@ -1,10 +1,10 @@
- // Initialize Firebase
- var config = {
-     apiKey: "AIzaSyDQJKbVvF6Wl7NYeiSWcuhrpnBFo5Qkf-w",
-     authDomain: "webassignment666.firebaseapp.com",
-     databaseURL: "https://webassignment666.firebaseio.com",
-     projectId: "webassignment666",
-     storageBucket: "webassignment666.appspot.com",
-     messagingSenderId: "224824077875"
- };
- firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDQJKbVvF6Wl7NYeiSWcuhrpnBFo5Qkf-w",
+    authDomain: "webassignment666.firebaseapp.com",
+    databaseURL: "https://webassignment666.firebaseio.com",
+    projectId: "webassignment666",
+    storageBucket: "webassignment666.appspot.com",
+    messagingSenderId: "224824077875"
+};
+firebase.initializeApp(config);
